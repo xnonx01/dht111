40b9c0a24f46f705b4676852fe984c4a111d62c0
xnonx01 <30832034+xnonx01@users.noreply.github.com>
2017-08-08 19:42:19 +0700

    Add files via upload

---
+++ o.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import dht11
+import datetime
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup
+while(1):
+	now=datetime.datetime.now()
+	date=now.strftime("%y-%m-%d: %H:%M:%s")
+	ins=dht11.DHT11(pin=14)
+	result=ins.read()
+	print(now)
+	print("Tem:%d c"%result.temperature)
+	print("Hum:%d %% "% result.humidity)
+	with open('pee.txt''a')as f:
+		fwrite("datetime:%s Hum:%d %%Tem :%d c\n"%(date.result.humidity.result.temperature))
+		time.sleep(2)
